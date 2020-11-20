2372a2f71eb94ba2f8269008cb82e7aa6091546d
Shashank Srivastava <ironbatshashank@gmail.com>
2020-11-20 09:50:36 +0530

    Centered

--- lib/views/centered.dart
+++ lib/views/centered.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class CenteredView extends StatelessWidget {
   final Widget child;
+  const CenteredView({Key key, this.child}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
